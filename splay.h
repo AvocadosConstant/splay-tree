41452b0f416c14f0fb358101e301962b36ecd442
Mariuxi Ana Yagual <mariuxiyagual@Mariuxis-MacBook-Air.local>
2015-04-27 10:35:21 -0400

    declared functions in class

--- splay.h
+++ splay.h
@@ -6,9 +6,21 @@
 
 class splay
 {
-	public:
-		splay();
-	private:
+ public:
+ splay():root(nullptr){};
+  ~splay();
+  void insert();
+  void inOrder();
+  node* search(double k);
+  void deleteKey(double k);
+  void sortedArray(); //?
+  
+ private:
+  node* root;
+  void insert(node *nd,double dta);
+  void inOrder(node *nd);
+  node* find(node* nd,double k);
+  void deleteKey(node* nd, double k);
 };
 
 #endif
